698a3363dc68b129280c01d6bce27f40a8790db1
romangurevitch <romang@jfrog.com>
2017-05-14 17:10:09 +0300

    Added timeout for Xray requests

--- xray/impl/src/main/java/com/jfrog/xray/client/impl/XrayClient.java
+++ xray/impl/src/main/java/com/jfrog/xray/client/impl/XrayClient.java
@@ -1,24 +1,14 @@
 package com.jfrog.xray.client.impl;
 
 import com.jfrog.xray.client.Xray;
-import org.apache.http.HttpHost;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.AuthCache;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.protocol.HttpClientContext;
-import org.apache.http.impl.auth.BasicScheme;
-import org.apache.http.impl.client.BasicAuthCache;
-import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
-
-import java.net.MalformedURLException;
-import java.net.URISyntaxException;
 
 /**
  * Created by romang on 2/2/17.
  */
 public class XrayClient {
+
+    private static int CONNECTION_TIMOUT = 300;
 
     static public Xray create(CloseableHttpClient preConfiguredClient, String url) {
         return new XrayImpl(preConfiguredClient, url);
@@ -31,6 +21,8 @@ public class XrayClient {
         XrayClientConfigurator configurator = new XrayClientConfigurator();
         configurator.setHostFromUrl(url);
         configurator.setCredentials(username, password, true);
+        configurator.setConnectTimeout(CONNECTION_TIMOUT);
+        configurator.setSocketTimeout(CONNECTION_TIMOUT);
 
         return new XrayImpl(configurator.getClient(), url);
     }
